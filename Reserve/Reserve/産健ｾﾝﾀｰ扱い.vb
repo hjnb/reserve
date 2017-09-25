9bf00cefe9e62c545df6dfc7fb4eeae3870125d9
DESKTOP-OJIT7DO\yoshi <yoshitaka7144@gmail.com>
2017-09-25 16:43:15 +0900

    産健センター扱いフォームの登録処理作成

--- Reserve/Reserve/産健ｾﾝﾀｰ扱い.vb
+++ Reserve/Reserve/産健ｾﾝﾀｰ扱い.vb
@@ -5,6 +5,28 @@ Public Class 産健ｾﾝﾀｰ扱い
 
     Public DB1 As String = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=C:\Users\yoshi\Desktop\Reserve.mdb"
 
+    Public preData As New List(Of SankenData)
+
+    Public Class SankenData
+        Private sankenName As String
+
+        Private sankenFlg As String
+
+        Public Sub New(ByVal sankenName As String, ByVal sankenFlg As String)
+            Me.sankenName = sankenName
+            Me.sankenFlg = sankenFlg
+        End Sub
+
+        Public Function getSankenName() As String
+            Return sankenName
+        End Function
+
+        Public Function getSankenFlg() As String
+            Return sankenFlg
+        End Function
+
+    End Class
+
     Private Sub 産健ｾﾝﾀｰ扱い_Disposed(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Disposed
         Form1.f_sanken = Nothing
     End Sub
@@ -12,6 +34,13 @@ Public Class 産健ｾﾝﾀｰ扱い
     Private Sub 産健ｾﾝﾀｰ扱い_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
         'リスト表示
         displaySankenList()
+
+        '初期状態を保持
+        Dim rowsCount As Integer = sankenDataGridView.Rows.Count
+        For i = 0 To rowsCount - 1
+            preData.Add(New SankenData(sankenDataGridView(0, i).Value, sankenDataGridView(1, i).Value))
+        Next
+
     End Sub
 
     Private Sub displaySankenList()
@@ -48,7 +77,7 @@ Public Class 産健ｾﾝﾀｰ扱い
         sankenDataGridView.Columns("Ind").HeaderText = "企業名"
         sankenDataGridView.Columns("Ind").Width = 165
         sankenDataGridView.Columns("Ind").HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
-        
+
         sankenDataGridView.Columns("Sanken").HeaderText = "該当"
         sankenDataGridView.Columns("Sanken").Width = 40
         sankenDataGridView.Columns("Sanken").HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter
@@ -93,6 +122,46 @@ Public Class 産健ｾﾝﾀｰ扱い
     End Sub
 
     Private Sub btnRegist_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnRegist.Click
+        Dim updateIndexList As New List(Of Integer)
+        Dim nowData As New List(Of SankenData)
+        Dim rowsCount As Integer = sankenDataGridView.Rows.Count
+        For i = 0 To rowsCount - 1
+            nowData.Add(New SankenData(sankenDataGridView(0, i).Value, If(IsDBNull(sankenDataGridView(1, i).Value), "", sankenDataGridView(1, i).Value)))
+            If nowData(i).getSankenName = preData(i).getSankenName AndAlso nowData(i).getSankenFlg <> preData(i).getSankenFlg Then
+                '初期状態と比較し異なる場合は対象のインデックスをリストに追加
+                updateIndexList.Add(i)
+            End If
+        Next
 
+        If updateIndexList.Count = 0 Then
+            MsgBox("変更がありません")
+        Else
+            '更新処理
+            updateSankenData(nowData, updateIndexList)
+
+            MsgBox(updateIndexList.Count & "件、変更しました")
+            '再表示
+            displaySankenList()
+
+            '初期状態の更新
+            preData.Clear()
+            For i = 0 To rowsCount - 1
+                preData.Add(New SankenData(nowData(i).getSankenName, nowData(i).getSankenFlg))
+            Next
+        End If
+
+    End Sub
+
+    Private Sub updateSankenData(ByVal nowSankenDataList As List(Of SankenData), ByVal updateIndexList As List(Of Integer))
+        Dim Cn As New OleDbConnection(DB1)
+        Dim SQLCm As OleDbCommand = Cn.CreateCommand
+
+        Cn.Open()
+        For Each i As Integer In updateIndexList
+            SQLCm.CommandText = "Update RsvD SET Sanken='" & nowSankenDataList(i).getSankenFlg & "' WHERE Ind='" & nowSankenDataList(i).getSankenName & "'"
+            SQLCm.ExecuteNonQuery()
+        Next
+        SQLCm.Dispose()
+        Cn.Close()
     End Sub
 End Class
